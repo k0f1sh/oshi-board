6124fd1cd3d8617ad1856bf80075fd7794f59731
k0f1sh <2574472+k0f1sh@users.noreply.github.com>
2021-04-17 10:00:43 +0900

    draggrable

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 import { Sprite } from './components/Sprite';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="App">
-        <div className="menu">
-          <div className="m-4 p-2 absolute right-0 top-0 bg-blue-500 text-white w-96 shadow">
-            <div className="mt-0 mb-2" style={{ borderBottom: "#fff 1px solid" }}>
-              <h1 className="font-bold text-xl">推しボード メニュー</h1>
-            </div>
+    <div className="App">
+      <div className="menu">
+        <div className="m-4 p-2 absolute right-0 top-0 bg-blue-500 text-white w-96 shadow">
+          <div className="mt-0 mb-2" style={{ borderBottom: "#fff 1px solid" }}>
+            <h1 className="font-bold text-xl">推しボード メニュー</h1>
           </div>
         </div>
-        {/* ------------ */}
-        <Sprite id={1} text={"あやちぇり！！！！"} size={30}></Sprite>
-      </div >
-    </DndProvider>
+      </div>
+      {/* ------------ */}
+      <Sprite id={1} text={"あやちぇり！！！！"} size={30}></Sprite>
+    </div >
   );
 }
 
--- src/components/Sprite.tsx
+++ src/components/Sprite.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import Draggable from "react-draggable";
 
 
 type Pos = {
@@ -13,7 +14,6 @@ type SpriteProps = {
 }
 
 export const Sprite: React.FC<SpriteProps> = ({ id, text, size }) => {
-    let { pos: { x, y }, handleMouseDown, handleMouseUp, handleMouseMove } = useDrag();
 
     const selectedStyle: React.CSSProperties = {
         borderStyle: "dashed",
@@ -24,8 +24,8 @@ export const Sprite: React.FC<SpriteProps> = ({ id, text, size }) => {
     const modelStyle: React.CSSProperties = {
         userSelect: "none",
         position: "absolute",
-        top: y,
-        left: x,
+        top: 0,
+        left: 0,
         display: "inline-block",
         padding: "1rem",
         fontSize: size.toString() + "px",
@@ -33,45 +33,11 @@ export const Sprite: React.FC<SpriteProps> = ({ id, text, size }) => {
 
     const style = { ...modelStyle, ...selectedStyle };
     return (
-        <div style={style} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} >
-            { text}
-        </div >
+        <Draggable>
+            <div style={style}>
+                {text}
+            </div >
+        </Draggable>
     );
 };
 
-
-type UseDragState = {
-    pos: Pos;
-    handleMouseDown: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-    handleMouseUp: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-    handleMouseMove: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-}
-
-const useDrag = (): UseDragState => {
-    const [isDragging, setIsDragging] = useState<boolean>(false);
-    const [pos, setPos] = useState<Pos>({ x: 0, y: 0 });
-    const [offset, setOffset] = useState<Pos>({ x: 0, y: 0 });
-
-    const handleMouseDown = useCallback((event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        setIsDragging(true);
-        setOffset({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY });
-        console.log({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY });
-    }, []);
-
-    const handleMouseUp = useCallback((event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        setIsDragging(false);
-    }, []);
-
-    const handleMouseMove = useCallback((event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        if (isDragging) {
-            setPos({ x: event.screenX, y: event.screenY });
-        };
-    }, [isDragging, offset]);
-
-    return {
-        pos,
-        handleMouseDown,
-        handleMouseUp,
-        handleMouseMove,
-    }
-};
